40ccca3f01516c4ec81a54f463872e78cc2bc218
innc_pb <pm@innc11.cn>
2019-06-24 13:29:54 +0800

    initial commit

---
+++ src/cn/innc11/AreaCommand/datatype/CmdArea.java
@@ -0,0 +1,17 @@
+package cn.innc11.AreaCommand.datatype;
+
+import cn.nukkit.level.Position;
+import cn.nukkit.math.Vector3;
+
+public class CmdArea
+{
+    public String world;
+
+    public Vector3 pos1;
+
+    public Vector3 pos2;
+
+    public String commandText = "";
+
+    public boolean isPlayerExecute = true;
+}
